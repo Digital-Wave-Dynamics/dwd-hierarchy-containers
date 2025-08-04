e5eedbbf5babe9cd8e9ad15c0217a06c831716c9
alittleton98 <30277771+alittleton98@users.noreply.github.com>
2025-08-04 10:07:36 -0500

    initial logic for the command script. Missed submitting in last check in

--- command_script.py
+++ command_script.py
@@ -1,3 +1,31 @@
-import time
-print("Hello World")
-time.sleep(1)
+from waapi import WaapiClient, CannotConnectToWaapiException
+from pprint import pprint
+import sys
+
+# Testing values
+#  '{6A980152-785A-4303-B16B-18FE2B343E33}', 
+#  'New Actor-Mixer', 
+#  'I am a note!', 
+#  'ActorMixer']
+
+# WAAPI
+
+id = '{6A980152-785A-4303-B16B-18FE2B343E33}'
+# Get ID from command argument list
+# head_object_id = sys.argv[1]
+# head_object_name = sys.argv[2]
+# head_object_note = sys.argv[3]
+
+try:
+    # Connect to WAAPI
+    with WaapiClient() as client:
+
+        waql_query = f'$ from object "{id}" select descendants'
+        args = {"waql" : waql_query}
+        options = { "return": ["name", "type"]}
+        result = client.call("ak.wwise.core.object.get", args, options=options)
+        pprint(result)
+except CannotConnectToWaapiException:
+    print("Could not connect to WAAPI")
+
+print ("Digital Wave Dynamics - Hierarchy Container Command/Script ran successfully!")
