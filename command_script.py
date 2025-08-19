358dbbe81f9a14b6c15af32259d1e3fc80e53211
alittleton98 <30277771+alittleton98@users.noreply.github.com>
2025-08-18 21:48:11 -0500

    Insert functionality works

--- command_script.py
+++ command_script.py
@@ -12,6 +12,8 @@ from waapi import WaapiClient, CannotConnectToWaapiException
 parser = argparse.ArgumentParser(description="Digital Wave Dynamics Hierarchy Container script")
 parser.add_argument("-i", "--instruction", help="The operation to be performed by the script")
 parser.add_argument("-id", help="The id that represents the root object for the hierarchy")
+parser.add_argument("-name", help="The id that represents the name of the template")
+parser.add_argument("-u", "--user", help="Whether or not the container was created by the user", action="store_true")
 
 args = parser.parse_args()
 print(args)
@@ -20,6 +22,8 @@ instruction = args.instruction
 if instruction == None:
     instruction = "create"
 head_object_id = args.id
+container_name = args.name
+container_owner = args.user
 
 # recursive function for retrieving children
 def get_object_info(head_object_id, wappi_client):  
@@ -50,7 +54,11 @@ def get_object_info(head_object_id, wappi_client):
         
     return object
         
-        
+#recursive function for adding the children
+def create_object_with_info(head_object, parent_object_id, json_data, waapi_client):
+    object = None
+    return object
+
 print("HelloWorld!")
 
 # Write to a log on non-windows machines. Command will route to the Wwise Log on Windows
@@ -64,7 +72,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 
 commands_filepath = 'commands.json'                             
-user_containers_filepath = 'user_containers.json'
+user_containers_dir = 'containers'
 
 # Commands
 if instruction == "create":
@@ -78,16 +86,16 @@ if instruction == "create":
     head_object = get_object_info(head_object_id, client)
     print(head_object)
 
-    with open(f"containers\\user_{head_object["name"]}_container.json", 'w') as user_containers_file:
+    with open(f"containers\\user_{head_object["name"]}.json", 'w') as user_containers_file:
             json.dump(head_object, user_containers_file, indent=4)
     
     # Assemble new commands 
-    command_id_child = f"dwd.insert.{ ''.join(head_object["name"].split()) }.child"     # Command ID used for when the hierarchy is inserted as a child object
-    command_id_parent = f"dwd.insert.{ ''.join(head_object["name"].split()) }.parent"   # Command ID used for when the hierarchy is inserted as a parent object
+    command_id_child = f"user.insert.{ ''.join(head_object["name"].split()) }.child"     # Command ID used for when the hierarchy is inserted as a child object
+    command_id_parent = f"user.insert.{ ''.join(head_object["name"].split()) }.parent"   # Command ID used for when the hierarchy is inserted as a parent object
     command_display_name = head_object["name"] # Leave white space for the name as presented in Wwise
     command_program = "python"
     command_startUpMode = "SingleSelectionSingleProcess"
-    command_arguments = f"\"${{CurrentCommandDirectory}}\\DigitalWaveDynamics\\dwd-hierarchy-containers\\command_script.py\" insert {head_object["name"]} ${{id}} ${{name}} ${{notes}} ${{type}}"
+    command_arguments = f"\"${{CurrentCommandDirectory}}\\DigitalWaveDynamics\\dwd-hierarchy-containers\\command_script.py\" -i insert -name {head_object["name"]} -id ${{id}} --user"
     command_redirectOutputs = True
     command_contextMenu_NewChild = {
                 "basePath": "DigitalWaveDynamics/New Child",
@@ -143,8 +151,35 @@ if instruction == "create":
         log_file.close()
     
 if instruction == "insert":
-    print()
-    
+    if container_name == None:
+        print("Insert command provided without container name")
+        exit()
+        
+    #recursively assemble the tree from the root object
+    try:
+        client = WaapiClient()
+    except CannotConnectToWaapiException:
+        print("Couldn't connect to WAAPI. Terminating program")
+        exit()
+        
+    if container_owner:
+        try:
+            with open(f"containers\\user_{container_name}.json", "r") as container_file:
+                container_json = json.load(container_file)
+                waapi_args = {
+                    "name" : container_json['name'], 
+                    "type": container_json['type'], 
+                    "notes" : container_json['notes'],
+                    "parent" : head_object_id,
+                    "children" : container_json['children']
+                    }
+                waapi_command = "ak.wwise.core.object.create"
+                client.call(waapi_command, waapi_args)
+        except FileNotFoundError:
+            print("Error: The file does not exist.")
+            exit()
+        
 if platform.system() != 'Windows':
     log_file.close()
         
+client.disconnect()
